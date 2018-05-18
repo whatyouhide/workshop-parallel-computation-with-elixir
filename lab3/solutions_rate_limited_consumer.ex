3017db8a92f1fc0d62c7d44290825dd5a0c776c3
Andrea Leopardi <an.leopardi@gmail.com>
2018-05-18 13:58:53 -0400

    Add more exercises

---
+++ lab3/solutions_rate_limited_consumer.ex
@@ -0,0 +1,60 @@
+defmodule Lab3.RateLimitedConsumer do
+  use GenStage
+
+  @interval 1000
+
+  def init(_) do
+    {:consumer, _producers = %{}}
+  end
+
+  def handle_subscribe(:producer, opts, from, producers) do
+    # We will only allow max_demand events every 5000 milliseconds
+    pending = opts[:max_demand] || 1000
+
+    # Register the producer in the state
+    producers = Map.put(producers, from, pending)
+    # Ask for the pending events and schedule the next time around
+    producers = ask_and_schedule(producers, from)
+
+    # Returns manual as we want control over the demand
+    {:manual, producers}
+  end
+
+  def handle_cancel(_, from, producers) do
+    # Remove the producers from the map on unsubscribe
+    {:noreply, [], Map.delete(producers, from)}
+  end
+
+  def handle_events(events, from, producers) do
+    # Bump the amount of pending events for the given producer
+    producers = Map.update!(producers, from, fn pending -> pending + length(events) end)
+
+    # Consume the events.
+    Enum.each(events, &process_event/1)
+
+    # A producer_consumer would return the processed events here.
+    {:noreply, [], producers}
+  end
+
+  def handle_info({:ask, from}, producers) do
+    # This callback is invoked by the Process.send_after/3 message below.
+    {:noreply, [], ask_and_schedule(producers, from)}
+  end
+
+  # Should ask the producer ("from") for mode demand with GenStage.ask/2, and schedule the next
+  # time it should ask.
+  defp ask_and_schedule(producers, from) do
+    case producers do
+      %{^from => pending} ->
+        GenStage.ask(from, pending)
+        Process.send_after(self(), {:ask, from}, @interval)
+        Map.put(producers, from, 0)
+      %{} ->
+        producers
+    end
+  end
+
+  defp process_event(event) do
+    IO.inspect(event)
+  end
+end
--- lab3/test/lab3_test.exs
+++ lab3/test/lab3_test.exs
@@ -1,4 +1,4 @@
-defmodule Lab3Test do
+defmodule Lab3.PartitionDispatcherTest do
   use ExUnit.Case
 
   defmodule InvoiceConsumer do
@@ -33,6 +33,42 @@ defmodule Lab3Test do
     for region <- regions do
       assert {:ok, _} = InvoiceConsumer.start_link(region)
     end
+
+    Process.sleep(2000)
+  end
+end
+
+defmodule Lab3.RateLimitedConsumerTest do
+  use ExUnit.Case
+
+  defmodule Producer do
+    use GenStage
+
+    @interval 1000
+
+    def init(_) do
+      {:producer, 0}
+    end
+
+    def handle_demand(demand, last_asked) do
+      if now() - last_asked < (@interval / 2) do
+        raise "failed"
+      else
+        {:noreply, Enum.take(Stream.repeatedly(&:rand.uniform/0), demand), now()}
+      end
+    end
+
+    defp now() do
+      System.system_time(:milliseconds)
+    end
+  end
+
+  @tag :skip
+  test "rate limiting" do
+    {:ok, producer} = GenStage.start_link(Producer, nil)
+    {:ok, consumer} = GenStage.start_link(Lab3.RateLimitedConsumer, nil)
+
+    GenStage.sync_subscribe(consumer, to: producer, max_demand: 10)
 
     Process.sleep(2000)
   end
